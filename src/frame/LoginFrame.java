ef21ffc3d4f38aaed9d5babc98227da04e59cbde
sangxiancheng <34865777+sangxiancheng@users.noreply.github.com>
2018-05-21 17:34:17 +0800

    2
    2

---
+++ src/frame/LoginFrame.java
@@ -0,0 +1,81 @@
+package frame;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+public class LoginFrame extends JFrame {
+	private JLabel userNameLabel;
+	private JLabel passwordLabel;
+	private JTextField userName;
+	private JPasswordField password;
+	private JButton login;
+	private JButton exit;
+	
+	public LoginFrame(){
+		this.setTitle("进销存管理系统--登录");
+		final JPanel panel = new JPanel();
+		panel.setSize(360, 230);
+		panel.setLayout(null);
+		getContentPane().add(panel);
+		setBounds(240,150,panel.getWidth(),panel.getHeight());
+		userNameLabel = new JLabel();
+		userNameLabel.setText("用户名:");
+		userNameLabel.setBounds(78, 60, 48, 18);
+		panel.add(userNameLabel);
+		userName = new JTextField();
+		userName.setBounds(136, 60, 120, 18);
+		panel.add(userName);
+		passwordLabel = new JLabel();
+		passwordLabel.setText("\u5BC6  \u7801:");//登录Label
+		passwordLabel.setBounds(78, 95, 48, 18);
+		panel.add(passwordLabel);
+		JPasswordField password = new JPasswordField();
+		password.addKeyListener(new KeyAdapter(){//密码输入长度不小于6
+			public void keyPressed(final KeyEvent e){
+				if(e.getKeyCode() >= 6)
+					login.doClick();
+			}
+		});
+		password.setBounds(136, 95, 120, 18);
+		panel.add(password);
+		login = new JButton();
+		login.addActionListener(new ActionListener(){//登录按钮事件
+			public void actionPerformed(final ActionEvent e){
+				
+			}
+		});
+		login.setText("登录");
+		login.setBounds(126, 134, 60, 18);
+		panel.add(login);
+		exit = new JButton();
+		exit.addActionListener(new ActionListener(){
+			public void actionPerformed(final ActionEvent e){
+				System.exit(0);
+			}
+		});
+		exit.setText("退出");
+		exit.setBounds(196, 134, 60, 18);
+		panel.add(exit);
+		getContentPane().add(panel);
+		this.setPreferredSize(new Dimension(250, 170));
+		this.setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+			JFrame loginFrame =	new LoginFrame();
+	}
+	
+}
--- src/frame/MainFrame.java
+++ src/frame/MainFrame.java
@@ -1,5 +1,110 @@
 package frame;
 
-public class MainFrame {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyVetoException;
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import javax.swing.border.BevelBorder;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
+
+public class MainFrame {
+	
+	private JFrame mainFrame;
+	private JDesktopPane desktopPane;
+	private Map<String,JInternalFrame> ifs = new HashMap<String,JInternalFrame>();
+	
+	public MainFrame(){
+		mainFrame = new JFrame("进销存管理系统");
+		mainFrame.getContentPane().setBackground(new Color(1790,188,120));
+		//mainFrame.addComponentListener(new FrameListener());
+		mainFrame.getContentPane().setLayout(new BorderLayout());
+		mainFrame.setBounds(100, 100, 800, 600);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.setFocusable(false);
+		tabbedPane.setBackground(new Color(211,230,192));
+		tabbedPane.setBorder(new BevelBorder(BevelBorder.RAISED));
+		
+		JPanel baseManagePanel = new JPanel();
+		baseManagePanel.setBackground(new Color(215,223,194));
+		baseManagePanel.setLayout(new BoxLayout(baseManagePanel,BoxLayout.X_AXIS));
+		baseManagePanel.add(createButton("客户信息管理",""));
+		baseManagePanel.add(createButton("商品信息管理",""));
+		baseManagePanel.add(createButton("供应商信息管理",""));
+		
+		
+	}
+	
+	private JButton createButton(String fName,String cName){
+		JButton button = new JButton();
+		Action action = new openFrameAction(fName,cName);
+		return button;
+	}
+	
+	protected final class openFrameAction extends AbstractAction {
+		private String frameName = null;
+		public openFrameAction(String cName,String frameName){
+			this.frameName = frameName;
+			putValue(Action.NAME,cName);
+			putValue(Action.SHORT_DESCRIPTION,cName);
+		}
+		public void actionPerformed(final ActionEvent e){
+			JInternalFrame jf = getIFrame(frameName);
+			jf.addInternalFrameListener(new InternalFrameAdapter(){
+				public void internalFrameClosed(InternalFrameEvent e){
+					ifs.remove(frameName);
+				}
+			});
+			if(jf.getDesktopPane() == null){
+				desktopPane.add(jf);
+				jf.setVisible(true);
+			}
+			try{
+				jf.setSelected(true);
+			}catch(PropertyVetoException e1){
+				e1.printStackTrace();
+			}
+		}
+	}
+	static {
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private JInternalFrame getIFrame(String frameName){
+		JInternalFrame jf = null;
+		if(!ifs.containsKey(frameName)){
+			try{
+				Class fClass = Class.forName(""+frameName);
+				Constructor constructor = fClass.getConstructor();
+				jf = (JInternalFrame)constructor.newInstance(null);
+				ifs.put(frameName, jf);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else
+			jf = ifs.get(frameName);
+		return jf;
+	}
+	
 }
